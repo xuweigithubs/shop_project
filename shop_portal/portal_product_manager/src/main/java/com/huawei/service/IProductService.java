cd9c3162b81166110c37e073d7d1c5ba663442cb
xuweigithubs <1101516243@qq.com>
2019-05-30 23:28:48 +0800

    SpringBoot项目初始提交

--- shop_portal/portal_product_manager/src/main/java/com/huawei/service/IProductService.java
+++ shop_portal/portal_product_manager/src/main/java/com/huawei/service/IProductService.java
@@ -1,5 +1,27 @@
 package com.huawei.service;
 
+import com.huawei.vo.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    /**
+     * 获取商品列表
+     * @param product
+     * @return
+     */
+    List<Product> selectProductByCondition(Product product);
+    /**
+     * 添加商品
+     */
+    void  addProduct(Product product);
+    /**
+     * 修改商品
+     */
+    void updateProduct(Product product);
+    /**
+     * 删除商品
+     */
+    void deleteProduct(Product product);
 
 }
---
+++ shop_portal/portal_product_manager/src/main/java/com/huawei/service/impl/ProductServiceImpl.java
@@ -0,0 +1,63 @@
+package com.huawei.service.impl;
+
+import com.huawei.dao.ProductMapper;
+import com.huawei.service.IProductService;
+import com.huawei.vo.Product;
+import com.huawei.vo.ProductCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service(value = "productService")
+public class ProductServiceImpl implements IProductService {
+    @Autowired
+    private ProductMapper productMapper;
+
+    /**
+     * 通过条件查询商品
+     * @param product
+     * @return
+     */
+    @Override
+    public List<Product> selectProductByCondition(Product product) {
+        ProductCondition condition=new ProductCondition();
+        //根据商品名称查询商品
+        if(null!=product.getProductname()){
+            condition.createCriteria().andProductnameLike(product.getProductname());
+        }
+        //根据商品ID查询商品
+        if(null!=product.getId()){
+            condition.createCriteria().andIdEqualTo(product.getId());
+        }
+        List<Product> productList=productMapper.selectByCondition(condition);
+
+        return productList;
+    }
+
+    /**
+     * 添加商品
+     * @param product
+     */
+    @Override
+    public void addProduct(Product product) {
+        productMapper.insertSelective(product);
+    }
+
+    /**
+     * 修改商品
+     * @param product
+     */
+    @Override
+    public void updateProduct(Product product) {
+        productMapper.updateByPrimaryKey(product);
+    }
+
+    /**
+     * 删除商品
+     * @param product
+     */
+    @Override
+    public void deleteProduct(Product product) {
+        productMapper.deleteByPrimaryKey(product.getId());
+    }
+}

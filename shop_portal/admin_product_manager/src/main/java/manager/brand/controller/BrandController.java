f3404f1290a6085aaf5884e1fead537a386f4d8a
xuweigithubs <1101516243@qq.com>
2020-11-24 00:15:08 +0800

    提交商品规格参数代码

--- shop_portal/admin_product_manager/src/main/java/manager/brand/controller/BrandController.java
+++ shop_portal/admin_product_manager/src/main/java/manager/brand/controller/BrandController.java
@@ -1,4 +1,7 @@
 package manager.brand.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BrandController {
 }
--- shop_portal/admin_product_manager/src/main/java/manager/brand/vo/BrandVO.java
+++ shop_portal/admin_product_manager/src/main/java/manager/brand/vo/BrandVO.java
@@ -1,4 +1,11 @@
 package manager.brand.vo;
 
+import lombok.Data;
+
+@Data
 public class BrandVO {
+  private String id;
+  private String name;
+  private String image;
+  private String letter;
 }

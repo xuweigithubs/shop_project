b66176082b187a8b5d91e90c173be1c173d6e30d
xuweigithubs <1101516243@qq.com>
2021-03-27 10:08:20 +0800

    提交商品规格参数代码

--- shop_portal/admin_product_manager/src/main/java/manager/spec/group/dao/SpecificationDao.java
+++ shop_portal/admin_product_manager/src/main/java/manager/spec/group/dao/SpecificationDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface SpecificationDao {
     //根据名称搜索参数组
-    List<SpecGroupVO> selectByCondition(SpecificationVO specificationVO);
+    List<SpecificationVO> selectByCondition(SpecificationVO specificationVO);
     //添加分组
     void add(SpecificationVO specificationVO);
     //修改分组
--- shop_portal/admin_product_manager/src/main/java/manager/spec/params/service/impl/ParamServiceImpl.java
+++ shop_portal/admin_product_manager/src/main/java/manager/spec/params/service/impl/ParamServiceImpl.java
@@ -2,25 +2,27 @@ package manager.spec.params.service.impl;
 import com.google.gson.Gson;
 import manager.common.PageView;
 import manager.spec.group.dao.SpecGroupDao;
+import manager.spec.group.dao.SpecificationDao;
 import manager.spec.group.vo.SpecGroupVO;
+import manager.spec.group.vo.SpecificationVO;
 import manager.spec.params.dao.ParamDao;
 import manager.spec.params.service.ParamService;
 import manager.spec.params.vo.SpecParamVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 @Service(value = "paramService")
 public class ParamServiceImpl implements ParamService {
     @Autowired
     private ParamDao paramDao;
     @Autowired
     private SpecGroupDao specGroupDao;
+    @Autowired
+    private SpecificationDao specificationDao;
 
     public PageView<List<SpecParamVO>> selectByConditionPage(SpecParamVO specParamVO) {
         PageView<List<SpecParamVO>> PageView=new PageView<List<SpecParamVO>>();
@@ -62,27 +64,37 @@ public class ParamServiceImpl implements ParamService {
             specGroupVO.setCid(cid);
             List<SpecGroupVO> categorySpecGroupList=specGroupDao.selectByCondition(specGroupVO);
             //拿出所有组的id,进行批量查询组下面的规格参数
-            List<String> groupIds=categorySpecGroupList.parallelStream().map(SpecGroupVO::getId).collect(Collectors.toList());
+            //List<String> groupIds=categorySpecGroupList.parallelStream().map(SpecGroupVO::getId).collect(Collectors.toList());
             specParamVO=new SpecParamVO();
-            specParamVO.setGroupIds(groupIds);
+            specParamVO.setCid(cid);
+            //specParamVO.setGroupIds(groupIds);
             //查询所有规格参数
             List<SpecParamVO> specParamsList=paramDao.selectByCondition(specParamVO);
             List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+            Map<String,Object> result=new LinkedHashMap<String,Object>();
             categorySpecGroupList.forEach(item->{
                 Map<String,Object> map=new LinkedHashMap<String,Object>();
-                List<String> paramNameList=specParamsList.stream().filter(curItem->{
+                List<SpecParamVO> paramNameList=specParamsList.stream().filter(curItem->{
                     if(null==curItem.getGroupId()){
                         return false;
                     }
                     return curItem.getGroupId().equals(item.getId());
-                }).map(SpecParamVO::getName).collect(Collectors.toList());
+                }).collect(Collectors.toList());
                 map.put("group",item.getName());
                 map.put("params",paramNameList);
                 list.add(map);
             });
+            result.put("groupParams",list);
+            //找到当前分类下没有关联分组的参数
+            List<SpecParamVO> noReleateResult=specParamsList.stream().filter(item->item.getGroupId()==null).collect(Collectors.toList());
+            result.put("params",noReleateResult);
             Gson gson=new Gson();
-            String tempStr=gson.toJson(list);
-            
+            String tempStr=gson.toJson(result);
+            SpecificationVO specificationVO=new SpecificationVO();
+            specificationVO.setCategoryId(cid);
+            specificationVO.setSpecifications(tempStr);
+            specificationDao.deleteByCondition(specificationVO);
+            specificationDao.add(specificationVO);
         }
     }
 }

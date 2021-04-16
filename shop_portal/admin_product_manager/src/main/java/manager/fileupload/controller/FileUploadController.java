21bf60447a00d4a6230ade64112d58dc1312a51b
xuweigithubs <1101516243@qq.com>
2021-04-16 13:32:09 +0800

    提交商品规格参数代码

--- shop_portal/admin_product_manager/src/main/java/manager/fileupload/controller/FileUploadController.java
+++ shop_portal/admin_product_manager/src/main/java/manager/fileupload/controller/FileUploadController.java
@@ -5,9 +5,15 @@ import manager.fileupload.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = {"/file"})
 @Api(value = "商品模块")
@@ -21,8 +27,8 @@ public class FileUploadController {
     private String path;
     @Value("${web.domain}")
     private String domain;
-    @RequestMapping("upload")
-    public String upload(MultipartFile file,String orgUrl){
+    @RequestMapping(value = "upload",method = RequestMethod.POST)
+    public  Map<String,String> upload(MultipartFile file,String index, String orgUrl){
         // 上传成功或者失败的提示
         String orgFileName="";
         if(orgUrl!=null){
@@ -31,7 +37,11 @@ public class FileUploadController {
         String fileName= FileNameUtils.getFileName(file.getOriginalFilename());
         //删除原来的文件
         FileUtils.deleteFile(path,orgFileName);
-        return FileUtils.upload(file,domain,path,fileName);
+        ;
+        Map<String,String> resultMap=new HashMap<String,String>();
+        resultMap.put("index",index);
+        resultMap.put("url",FileUtils.upload(file,domain,path,fileName));
+        return resultMap;
     }
 
 }
